5dffb8b79d18ce390e70fb78bccd6f7958ab0bbb
blr-Oliver <vasily.liaskovsky@gmail.com>
2018-10-16 10:11:48 +0300

    shape tests

--- src/promise.spec.ts
+++ src/promise.spec.ts
@@ -1,6 +1,42 @@
 import 'mocha';
+import {expect} from 'chai';
 
-describe('Promise', () => {
-  it('can run tests', () => {
-  })
-});
+((subj) => {
+  describe('Promise', () => {
+    describe('Shape and typing', () => {
+      describe('Static', () => {
+        it('is defined', () => {
+          expect(subj).not.undefined;
+        });
+        it('has method "resolve"', () => {
+          expect(subj.resolve).to.be.a('function');
+        });
+        it('has method "reject"', () => {
+          expect(subj.reject).to.be.a('function');
+        });
+        it('has method "all"', () => {
+          expect(subj.all).to.be.a('function');
+        });
+        it('has method "race"', () => {
+          expect(subj.race).to.be.a('function');
+        });
+      });
+      describe('Prototype', () => {
+        const proto = subj.prototype;
+        it('is defined', () => {
+          expect(proto).not.undefined;
+        });
+        it('has method "then"', () => {
+          expect(proto.then).to.be.a('function');
+        });
+        it('has method "catch"', () => {
+          expect(proto['catch']).to.be.a('function');
+        });
+        it('has method "finally"', () => {
+          // this one is failing for native Promise (node v8.11.3)
+          expect(proto['finally']).to.be.a('function');
+        });
+      });
+    })
+  });
+})(Promise);
