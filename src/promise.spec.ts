6bdf3c899193163c08d5831d2999e0b09bf561e2
blr-Oliver <vasily.liaskovsky@gmail.com>
2018-10-16 11:53:48 +0300

    fulfillment tests

--- src/promise.spec.ts
+++ src/promise.spec.ts
@@ -1,6 +1,22 @@
 import 'mocha';
 import {expect} from 'chai';
 
+class ManualResolver {
+  resolve: Function;
+  reject: Function;
+
+  constructor() {
+    this.task = this.task.bind(this);
+  }
+
+  task(resolve: Function, reject: Function) {
+    this.resolve = resolve;
+    this.reject = reject;
+  }
+}
+
+const runDelayed = (cb) => setTimeout(cb, 0);
+
 ((subj) => {
   describe('Promise', () => {
     describe('Shape and typing', () => {
@@ -32,10 +48,99 @@ import {expect} from 'chai';
         it('has method "catch"', () => {
           expect(proto['catch']).to.be.a('function');
         });
-        it('has method "finally"', () => {
-          // this one is failing for native Promise (node v8.11.3)
+        xit('has method "finally"', () => {
+          // fails for native Promise (node v8.11.3)
           expect(proto['finally']).to.be.a('function');
         });
+      });
+    });
+    describe('Execution', () => {
+      let resolver, promise;
+      beforeEach(() => {
+        resolver = new ManualResolver();
+        promise = new subj(resolver.task);
+      });
+
+      it('executes passed task immediately', () => {
+        let executed = false;
+        new subj(() => {
+          executed = true;
+        });
+        expect(executed).to.be.equal(true);
+      });
+      it('provides resolve callback', () => {
+        expect(resolver.resolve).to.be.a('function');
+      });
+      it('provides reject callback', () => {
+        expect(resolver.reject).to.be.a('function');
+      });
+      it('never fires a handler before resolution', (done) => {
+        let executed = false;
+        const handler = () => executed = true;
+        promise.then(handler, handler);
+        promise['catch'](handler);
+        promise['finally'] && promise['finally'](handler);
+        runDelayed(() => {
+          expect(executed).to.be.equal(false);
+          done();
+        });
+      });
+      it('can accept null handlers', () => {
+        promise.then(null, null);
+        promise['catch'](null);
+      });
+      describe('Fulfillment', () => {
+        let count = 0, value, firstToExecute = 0;
+        before(() => {
+          promise.then((arg) => {
+            ++count;
+            value = arg;
+            if (!firstToExecute)
+              firstToExecute = 1;
+          });
+          promise.then(() => {
+            if (!firstToExecute)
+              firstToExecute = 2;
+          });
+          resolver.resolve(42);
+        });
+
+        it('eventually fires handler exactly once', (done) => {
+          runDelayed(() => {
+            expect(count).to.be.equal(1);
+            done();
+          });
+        });
+        it('passes resolution value to the handler', (done) => {
+          runDelayed(() => {
+            expect(value).to.be.equal(42);
+            done();
+          });
+        });
+        it('ignores subsequent fulfillments', (done) => {
+          resolver.resolve(1984);
+          runDelayed(() => {
+            expect(count).to.be.equal(1);
+            expect(value).to.be.equal(42);
+            done();
+          });
+        });
+        xit('ignores subsequent rejections', (done) => {
+          // fails for native Promise (node v8.11.3)
+          let executed = false;
+          promise['catch'](() => executed = true);
+          resolver.reject('ignored');
+          runDelayed(() => {
+            expect(executed).to.be.equal(false);
+            done();
+          });
+        });
+        it('executes handlers in order', (done) => {
+          runDelayed(() => {
+            expect(firstToExecute).to.be.equal(1);
+            done();
+          });
+        })
       });
     })
   });
